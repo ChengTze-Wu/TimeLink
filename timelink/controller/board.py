aecc748bc6aabbab94c38377e79b80d82e8cf23d
ChengTze-Wu <yaoop3050777@gmail.com>
2022-08-15 01:16:47 +0800

    fix board path

--- timelink/controller/board.py
+++ timelink/controller/board.py
@@ -1,29 +1,29 @@
 from flask import Blueprint, render_template
 from timelink.util.login_validator import login_required
 
-bp = Blueprint('board', __name__, url_prefix='/board')
+bp = Blueprint('board', __name__)
 
-@bp.route("/", methods=['GET'])
+@bp.route("/board", methods=['GET'])
 @login_required
 def index():
     return render_template('board/board.html')
 
-@bp.route("/service", methods=['GET'])
+@bp.route("/board/service", methods=['GET'])
 @login_required
 def service():
     return render_template('board/service.html')
 
-@bp.route("/reserve", methods=['GET'])
+@bp.route("/board/reserve", methods=['GET'])
 @login_required
 def reserve():
     return render_template('board/reserve.html')
 
-@bp.route("/group", methods=['GET'])
+@bp.route("/board/group", methods=['GET'])
 @login_required
 def group():
     return render_template('board/group.html')
 
-@bp.route("/member", methods=['GET'])
+@bp.route("/board/member", methods=['GET'])
 @login_required
 def member():
     return render_template('board/member.html')
