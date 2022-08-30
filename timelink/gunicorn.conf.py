fee2d2b47d22a2d7bbbc46fc6fedd17e793b88ae
ChengTze-Wu <yaoop3050777@gmail.com>
2022-08-30 20:40:52 +0800

    add gunicorn conf and log file

---
+++ timelink/gunicorn.conf.py
@@ -0,0 +1,7 @@
+bind = "127.0.0.1:8000"
+workers = 2
+
+# log
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+accesslog = "./log/access.log"
+errorlog = "./log/error.log"
