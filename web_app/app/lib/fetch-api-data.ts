1fdfe4a371788368da86eca30358edf7a8bafcc9
ChengTze-Wu <yaoop3050777@gmail.com>
2023-12-13 19:02:08 +0800

    Add fetch-api-data module

---
+++ web_app/app/lib/fetch-api-data.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { unstable_noStore as noStore } from "next/cache";
+
+export async function get(apiEndpoint: string) {
+  noStore();
+  try {
+    const res = await fetch(apiEndpoint, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return res.json();
+  } catch (error) {
+    console.error(`Error fetching ${apiEndpoint}: ${error}`);
+    throw new Error("Failed to fetch API data");
+  }
+}
