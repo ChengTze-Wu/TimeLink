da3d087464ad43e7862befb50cb148b16b71124e
ChengTze-Wu <yaoop3050777@gmail.com>
2023-12-18 18:20:45 +0800

    Refactor RESTfulResponse class to improve readability and maintainability

--- web_api/views/response_view.py
+++ web_api/views/response_view.py
@@ -1,44 +1,55 @@
-from web_api.db.models import BaseModel
-from typing import List, Tuple
+from typing import Tuple, Optional
 
-class RESTfulResponse():
-    def __init__(self, model: List[BaseModel]|BaseModel, hide_fields: list=[], pagination: Tuple[int,int,int]=(None,None,None)):
-        self.__model = model
-        self.__hide_fields = hide_fields
-        self.__pagination = pagination  # (page, per_page, total_items)
 
-    def __hide_fields_handler(self, model: BaseModel) -> dict:
-        dict_data = model.to_dict()
-        for field in self.__hide_fields:
-            if field in dict_data:
-                dict_data.pop(field)
-        return dict_data
+class RESTfulResponse:
+    def __init__(
+        self,
+        data: Optional[dict | list] = None,
+        hide_fields: Optional[list] = None,
+        pagination: Optional[Tuple[int, int, int]] = None, # (page, per_page, total_items)
+    ):
+        if data and not isinstance(data, (dict, list)):
+            raise TypeError("data type must be dict, list")
 
-    def __pagination_handler(self, data: List[dict]) -> dict:
+        self.__data = data
+        self.__hide_fields = hide_fields if hide_fields is not None else []
+        self.__pagination = pagination
+
+    def __hide_fields_handler(self, data: dict) -> dict:
+        return {
+            key: value
+            for key, value in data.items()
+            if key not in self.__hide_fields
+        }
+
+    def __pagination_handler(self, dataset: list) -> dict:
         page, per_page, total_items = self.__pagination
-        current_page_items = len(data)
-        total_pages = total_items // per_page + 1 if total_items % per_page else total_items // per_page
+        current_page_items = len(dataset)
+        total_pages, has_remainder = divmod(total_items, per_page) if total_items > 0 else (1, 0)
+        if has_remainder:
+            total_pages += 1
         next_page = page + 1 if page < total_pages else None
         return {
-            'current_page': page,
-            'next_page': next_page,
-            'current_page_items': current_page_items,
-            'total_pages': total_pages,
-            'total_items': total_items,
+            "current_page": page,
+            "next_page": next_page,
+            "current_page_items": current_page_items,
+            "total_pages": total_pages,
+            "total_items": total_items,
         }
-    
+
     def __handle(self):
-        if isinstance(self.__model, list):
-            data = [self.__hide_fields_handler(model) for model in self.__model]
+        if isinstance(self.__data, list):
+            dataset = [self.__hide_fields_handler(data) for data in self.__data]
             if self.__pagination:
-                return {
-                    'data': data,
-                    'pagination': self.__pagination_handler(data)
-                }
-            return data
-        if isinstance(self.__model, object):
-            return self.__hide_fields_handler(self.__model)
+                return {"data": dataset, "pagination": self.__pagination_handler(dataset)}
+            return dataset
+        if isinstance(self.__data, object):
+            return self.__hide_fields_handler(self.__data)
         return {}
 
-    def to_dict(self) -> dict:
+    def to_serializable(self) -> dict | list:
+        if not self.__data:
+            if self.__pagination:
+                return {"data": [], "pagination": self.__pagination_handler([])}
+            return [] if isinstance(self.__data, list) else {}
         return self.__handle()
