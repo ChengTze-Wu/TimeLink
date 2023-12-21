a131ca3ecea863dc464f8cd11051a3fed0c25eee
ChengTze-Wu <yaoop3050777@gmail.com>
2023-12-21 16:27:59 +0800

    Refactor user service to use UserRepository

--- web_api/routers/user_router.py
+++ web_api/routers/user_router.py
@@ -1,105 +1,127 @@
 from flask import Blueprint, request, abort
-from web_api.utils.validators import RequestValidator, EmailValidator, PasswordValidator
+from web_api.validators.request_vaildator import RequestValidator
+from web_api.validators.field_validators import EmailValidator, PasswordValidator
 from web_api.views.response_view import RESTfulResponse
-from web_api.services import user_service
+from web_api.services.user_service import UserService
+from werkzeug.exceptions import HTTPException
 
 
 bp = Blueprint("user_router", __name__)
+user_service = UserService()
 
 
 @bp.route("", methods=["POST"])
 def register_endpoint():
-    request_validator = RequestValidator(request)
-    request_validator.config(
-        request_type="json",
-        required_fields=["email", "username", "password", "name"],
-        field_types={
-            "id": str,
-            "email": str,
-            "username": str,
-            "password": str,
-            "name": str,
-            "phone": str,
-            "is_active": bool,
-            "is_deleted": bool,
-        },
-        field_max_lengths={
-            "email": 100,
-            "username": 50,
-            "password": 100,
-            "name": 50,
-            "phone": 50,
-        },
-        field_validators={
-            "email": EmailValidator,
-            "password": PasswordValidator,
-        },
-    )
-    is_valid = request_validator.check()
-    if not is_valid:
-        abort(400, request_validator.message)
+    try:
+        request_validator = RequestValidator(
+            request_type="json",
+            required_fields=["email", "username", "password", "name"],
+            field_types={
+                "id": str,
+                "email": str,
+                "username": str,
+                "password": str,
+                "name": str,
+                "phone": str,
+                "is_active": bool,
+                "is_deleted": bool,
+            },
+            field_max_lengths={
+                "email": 100,
+                "username": 50,
+                "password": 100,
+                "name": 50,
+                "phone": 50,
+            },
+            field_validators={
+                "email": EmailValidator,
+                "password": PasswordValidator,
+            }
+        )
+        request_validator.check(request)
 
-    user_json_data = request.get_json()
-    created_user_data = user_service.create_one(user_json_data)
-    return RESTfulResponse(created_user_data, hide_fields=["password"]).to_serializable(), 201
+        user_json_data = request.get_json()
+        created_user_data = user_service.create(user_json_data)
+        return RESTfulResponse(created_user_data, hide_fields=["password"]).to_serializable(), 201
+    except HTTPException as e:
+        abort(e.code, e.description)
+    except Exception as e:
+        abort(500, e)
 
 
-@bp.route("/<user_id>", methods=["DELETE"])
+@bp.route("/<uuid:user_id>", methods=["DELETE"])
 def delete_endpoint(user_id):
-    deleted_user_data = user_service.logical_delete_by_id(user_id)
-    return RESTfulResponse(deleted_user_data, hide_fields=["password"]).to_serializable()
+    try:
+        deleted_user_data = user_service.delete(user_id)
+        return RESTfulResponse(deleted_user_data, hide_fields=["password"]).to_serializable()
+    except HTTPException as e:
+        abort(e.code, e.description)
+    except Exception as e:
+        abort(500, e)
 
 
-@bp.route("/<user_id>", methods=["PUT"])
+@bp.route("/<uuid:user_id>", methods=["PUT"])
 def update_endpoint(user_id):
-    request_validator = RequestValidator(request)
-    request_validator.config(
-        request_type="json",
-        field_types={
-            "email": str,
-            "username": str,
-            "password": str,
-            "name": str,
-            "phone": str,
-            "is_active": bool,
-            "is_deleted": bool,
-        },
-        field_max_lengths={
-            "email": 100,
-            "username": 50,
-            "password": 100,
-            "name": 50,
-            "phone": 50,
-        },
-        field_validators={
-            "email": EmailValidator,
-            "password": PasswordValidator,
-        },
-    )
-    is_valid = request_validator.check()
-    if not is_valid:
-        abort(400, request_validator.message)
+    try:
+        request_validator = RequestValidator(
+            request_type="json",
+            field_types={
+                "email": str,
+                "username": str,
+                "password": str,
+                "name": str,
+                "phone": str,
+                "is_active": bool,
+                "is_deleted": bool,
+            },
+            field_max_lengths={
+                "email": 100,
+                "username": 50,
+                "password": 100,
+                "name": 50,
+                "phone": 50,
+            },
+            field_validators={
+                "email": EmailValidator,
+                "password": PasswordValidator,
+            }
+        )
+        request_validator.check(request)
 
-    user_json_data = request.get_json()
-    updated_user_data = user_service.update_one_by_id(user_json_data, user_id)
-    return RESTfulResponse(updated_user_data, hide_fields=["password"]).to_serializable()
+        user_json_data = request.get_json()
+        updated_user_data = user_service.update(user_id, user_json_data)
+        return RESTfulResponse(updated_user_data, hide_fields=["password"]).to_serializable()
+    except HTTPException as e:
+        abort(e.code, e.description)
+    except Exception as e:
+        abort(500, e)
 
 
-@bp.route("/<user_id>", methods=["GET"])
+@bp.route("/<uuid:user_id>", methods=["GET"])
 def get_one_endpoint(user_id):
-    user_data = user_service.get_one_by_id(user_id)
-    return RESTfulResponse(user_data, hide_fields=["password"]).to_serializable()
+    try:
+        user_data = user_service.get_one(user_id)
+        return RESTfulResponse(user_data, hide_fields=["password"]).to_serializable()
+    except HTTPException as e:
+        abort(e.code, e.description)
+    except Exception as e:
+        abort(500, e)
 
 
 @bp.route("", methods=["GET"])
 def get_all_endpoint():
-    page = request.args.get("page", 1, type=int)
-    per_page = request.args.get("per_page", 10, type=int)
-    query = request.args.get("query", None, type=str)
-    status = request.args.get("status", None, type=int)
-    user_dataset, total_items_count = user_service.get_all_available_by_filter(page=page, per_page=per_page, query=query, status=status, with_total_items=True)
-    return RESTfulResponse(
-                user_dataset, 
-                hide_fields=["password", "is_deleted"], 
-                pagination=(page, per_page, total_items_count)
-            ).to_serializable()
+    try:
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 10, type=int)
+        query = request.args.get("query", None, type=str)
+        status = request.args.get("status", None, type=int)
+        user_dataset, total_items_count = user_service.get_list(page=page, per_page=per_page, query=query, status=status, with_total_items=True)
+        return RESTfulResponse(
+                    user_dataset, 
+                    hide_fields=["password", "is_deleted"], 
+                    pagination=(page, per_page, total_items_count)
+                ).to_serializable()
+    except HTTPException as e:
+        abort(e.code, e.description)
+    except Exception as e:
+        abort(500, e)
--- web_api/services/user_service.py
+++ web_api/services/user_service.py
@@ -1,139 +1,54 @@
-from flask import abort, current_app
-from sqlalchemy.exc import IntegrityError, SQLAlchemyError
-from sqlalchemy import select, or_
-from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.exceptions import NotFound, Conflict, Unauthorized, Forbidden
-from web_api.db.connect import Session
-from web_api.db.models import User
+from werkzeug.security import generate_password_hash
+from web_api.repositories.user_repository import UserRepository
 from typing import List, Tuple
-import jwt
-import datetime
 
 
-def create_one(user_data: dict) -> dict:
-    try:
-        user = User(
-            id=user_data.get("id"),
-            email=user_data.get("email"),
-            username=user_data.get("username"),
-            password=generate_password_hash(user_data.get("password")),
-            name=user_data.get("name"),
-            phone=user_data.get("phone"),
-            is_active=user_data.get("is_active"),
-            is_deleted=user_data.get("is_deleted"),
+class UserService:
+    def __init__(self):
+        self.repository = UserRepository()
+
+    def create(self, user_json_data: dict) -> dict:
+        new_user_data = {
+            "id": user_json_data.get("id"),
+            "email": user_json_data.get("email"),
+            "username": user_json_data.get("username"),
+            "password": generate_password_hash(user_json_data.get("password")),
+            "name": user_json_data.get("name"),
+            "line_user_id": user_json_data.get("line_user_id"),
+            "phone": user_json_data.get("phone"),
+            "is_active": user_json_data.get("is_active", True),
+            "is_deleted": user_json_data.get("is_deleted", False),
+        }
+        return self.repository.create_new_user(new_user_data)
+
+    def update(self, user_id: str, user_data: dict) -> dict:
+        user_data = {
+            "email": user_data.get("email"),
+            "username": user_data.get("username"),
+            "password": generate_password_hash(user_data.get("password"))
+            if user_data.get("password")
+            else None,
+            "name": user_data.get("name"),
+            "line_user_id": user_data.get("line_user_id"),
+            "phone": user_data.get("phone"),
+            "is_active": user_data.get("is_active"),
+            "is_deleted": user_data.get("is_deleted"),
+        }
+        return self.repository.update_user_by_id(user_id, user_data)
+
+    def delete(self, user_id: str) -> dict:
+        return self.repository.logical_delete_user_by_id(user_id)
+
+    def get_one(self, user_id: str) -> dict:
+        return self.repository.get_one_by_id(user_id)
+
+    def get_list(
+        self, page: int, per_page: int, query: str, status: int, with_total_items: bool
+    ) -> List[dict] | Tuple[List[dict], int]:
+        list_dict_users = self.repository.get_users_by_filter(
+            page, per_page, query, status
         )
-        with Session() as session:
-            session.add(user)
-            session.commit()
-            session.refresh(user)
-            return user.to_dict()
-    except SQLAlchemyError as e:
-        if isinstance(e, IntegrityError):
-            if "Key (email)" in str(e.orig):
-                abort(409, f"Email `{user_data['email']}` already exists")
-            if "Key (username)" in str(e.orig):
-                abort(409, f"Username `{user_data['username']}` already exists")
-        abort(500, e)
-    except Exception as e:
-        abort(500, e)
-
-
-def update_one_by_id(user_data: dict, user_id: str) -> dict:
-    try:
-        with Session() as session:
-            user = session.scalars(
-                select(User).filter(User.id == user_id)
-            ).first()
-            if user is None:
-                raise NotFound(f"User not found")
-
-            for field, value in user_data.items():
-                if hasattr(user, field):
-                    if getattr(user, field) != value:
-                        if field == "password":
-                            value = generate_password_hash(value)
-                        setattr(user, field, value)
-
-            session.commit()
-            session.refresh(user)
-            return user.to_dict()
-    except NotFound as e:
-        abort(404, e.description)
-    except SQLAlchemyError as e:
-        if isinstance(e, IntegrityError):
-            if "Key (email)" in str(e.orig):
-                abort(409, f"Email `{user_data['email']}` already exists")
-            if "Key (username)" in str(e.orig):
-                abort(409, f"Username `{user_data['username']}` already exists")
-    except Exception as e:
-        abort(500, e)
-
-
-def logical_delete_by_id(user_id: str) -> dict:
-    try:
-        with Session() as session:
-            user = session.query(User).filter(User.id == user_id).first()
-            if user is None:
-                raise NotFound(f"User not found")
-            if user.is_deleted:
-                raise Conflict(f"User `{user.username}` already deleted")
-            user.is_deleted = True
-            session.commit()
-            session.refresh(user)
-            return user.to_dict()
-    except Conflict as e:
-        abort(409, e.description)
-    except NotFound as e:
-        abort(404, e.description)
-    except Exception as e:
-        abort(500, e)
-
-
-def get_one_by_id(user_id: str) -> dict:
-    try:
-        with Session() as session:
-            user = session.scalars(
-                select(User).filter(User.id == user_id)
-            ).first()
-            if user is None:
-                raise NotFound(f"User not found")
-            return user.to_dict()
-    except NotFound as e:
-        abort(404, e.description)
-    except Exception as e:
-        abort(500, e)
-
-
-def get_all_available_by_filter(
-    page: int = 1, per_page: int = 10, query: str = None, status: int = None, with_total_items: bool = False
-) -> List[dict] | Tuple[List[dict], int]:
-    try:
-        with Session() as session:
-            base_query = select(User).filter(User.is_deleted == False).order_by(User.created_at.desc())
-            if status == 0:
-                base_query = base_query.filter(User.is_active == False)
-
-            if status == 1:
-                base_query = base_query.filter(User.is_active == True)
-
-            if query is not None:
-                search_filter = or_(
-                    User.email.ilike(f'%{query}%'),
-                    User.username.ilike(f'%{query}%'),
-                    User.name.ilike(f'%{query}%'),
-                    User.phone.ilike(f'%{query}%')
-                )
-                base_query = base_query.filter(search_filter)
-
-            users = session.scalars(
-                base_query.offset((page - 1) * per_page).limit(per_page)
-            ).all()
-            list_dict_users = [user.to_dict() for user in users]
-
-            if with_total_items is True:
-                total_items = len(session.scalars(base_query).all())
-                return list_dict_users, total_items
-
-            return list_dict_users
-    except Exception as e:
-        abort(500, e)
+        if with_total_items:
+            total_items_count = self.repository.count_all_users_by_filter(query, status)
+            return list_dict_users, total_items_count
+        return list_dict_users
