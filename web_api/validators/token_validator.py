0edd8eea58560f35318c3d38c68caca1343d54b1
ChengTze-Wu <yaoop3050777@gmail.com>
2024-01-02 18:38:28 +0800

    Update JWT role description

--- web_api/validators/token_validator.py
+++ web_api/validators/token_validator.py
@@ -16,7 +16,7 @@ def verify_access_token(access_roles: list=None, check_owner=False):
     Decorator for verifying jwt access token in HTTP Authorization Header Authorization with Bearer prefix.
         > Authorization: Bearer <JWT token>
 
-    If the jwt role is admin, it will pass all the verification.
+    If the JWT role is 'admin', it will pass all verifications.
 
     Basic Usage: only verify access token
     >>> @app.route("/api/users", methods=["GET"])
