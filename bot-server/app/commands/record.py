62ea32be81faffde31c0d9221a458a2eb7cf2f53
ChengTze-Wu <yaoop3050777@gmail.com>
2024-01-19 00:54:25 +0800

    Implement a demo commands

--- bot-server/app/commands/record.py
+++ bot-server/app/commands/record.py
@@ -8,7 +8,8 @@ class RecordCommand(Command):
     async def async_execute(self):
         user_id = self.event.source.user_id
         async with httpx.AsyncClient() as client:
-            user_resp = await client.get(f'{API_SERVER_HOST}/api/users/line/{user_id}')
+            headers = {'user-agent': 'TimeLink-Line-Bot/0.0.1'}
+            user_resp = await client.get(f'{API_SERVER_HOST}/api/users/line/{user_id}', timeout=2, headers=headers)
         user_json: dict = user_resp.json()
         appointments: list = user_json.get("appointments")
         return f"你共有 {len(appointments)} 筆預約"
