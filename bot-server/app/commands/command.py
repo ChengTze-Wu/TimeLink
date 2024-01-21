8314a66f1cb57460ff8c9adfde8c4e2442acc853
ChengTze-Wu <yaoop3050777@gmail.com>
2024-01-21 15:49:08 +0800

    Update Command class to handle PostbackEvent

--- bot-server/app/commands/command.py
+++ bot-server/app/commands/command.py
@@ -1,10 +1,16 @@
 from abc import ABC, abstractmethod
-from linebot.v3.webhooks import MessageEvent
+from linebot.v3.webhooks import (
+    MessageEvent,
+    PostbackEvent,
+)
+from linebot.v3.messaging import (
+    Message
+)
 
 class Command(ABC):
-    def __init__(self, event: MessageEvent):
+    def __init__(self, event: MessageEvent | PostbackEvent):
         self.event = event
 
     @abstractmethod
-    async def async_execute(self):
+    async def async_execute(self) -> Message | None:
         pass
