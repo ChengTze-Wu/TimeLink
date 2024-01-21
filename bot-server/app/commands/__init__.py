d1c9c0e504af9685e2388ad846a6de81f30848e3
ChengTze-Wu <yaoop3050777@gmail.com>
2024-01-21 20:59:54 +0800

    Let commands be package

---
+++ bot-server/app/commands/__init__.py
@@ -0,0 +1,6 @@
+from .record import RecordCommand
+from .reserve import ReserveCommand
+from .service import ServiceCommand
+from .default import DefaultCommand
+from .menu import MenuCommand
+from .command import Command
