a53d0a03891c64b87c9161c95a4d1fc175027618
ChengTze-Wu <yaoop3050777@gmail.com>
2024-01-21 15:48:52 +0800

    Update command selector for supporting PostbackEvent

--- bot-server/app/commands/selector.py
+++ bot-server/app/commands/selector.py
@@ -1,15 +1,16 @@
+from httpx import post
 from linebot.v3.webhooks import (
-    MessageEvent
+    MessageEvent,
+    PostbackEvent,
 )
 from .record import RecordCommand
 from .reserve import ReserveCommand
 from .service import ServiceCommand
+from .default import DefaultCommand
+from .menu import MenuCommand
 from .command import Command
 
-class DefaultCommand(Command):
-    async def async_execute(self):
-        pass
-            
+
 class CommandSelector:
     COMMAND_CALL_KEYWORD = "tl"
 
@@ -20,14 +21,18 @@ class CommandSelector:
             "預約": ReserveCommand
         }
 
-    def get_command(self, event: MessageEvent) -> Command:
-        message_text = event.message.text.lower()
-        
-        if self.COMMAND_CALL_KEYWORD not in message_text:
-            return DefaultCommand(event)
+    def get_command(self, event: MessageEvent | PostbackEvent) -> Command:
+        if isinstance(event, MessageEvent):
+            event_keyword = event.message.text.lower()
+
+            if self.COMMAND_CALL_KEYWORD not in event_keyword:
+                return DefaultCommand(event)
+
+        if isinstance(event, PostbackEvent):
+            event_keyword = event.postback.data.lower()
         
         for key, command_cls in self.strategies.items():
-            if key in message_text:
+            if key in event_keyword:
                 return command_cls(event)
             
-        return DefaultCommand(event)
+        return MenuCommand(event)
