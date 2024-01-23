aff2dcc4954184ee6255386caca80ef98ccd2382
ChengTze-Wu <yaoop3050777@gmail.com>
2024-01-23 18:09:19 +0800

    Update GROUP_NOT_LINKED URL in messages.py

--- bot-server/app/messages.py
+++ bot-server/app/messages.py
@@ -9,6 +9,6 @@ class ViewMessage:
     
     # template messages
     WELCOME_NEW_USER = Template("歡迎, 您尚未加入系統, 請至 LIFF 註冊.")
-    GROUP_NOT_LINKED = Template("此群組尚未連結 Bot，請至 https://timelink.cc 群組管理輸入群組 ID：$line_group_id")
+    GROUP_NOT_LINKED = Template("此群組尚未連結 Bot，請至 https://timelink.chengtze.cc 群組管理輸入群組 ID：$line_group_id")
     RECORD = Template("你共有 $count 筆預約\n$appointments")
     NO_RECORD = "你尚未有預約"
