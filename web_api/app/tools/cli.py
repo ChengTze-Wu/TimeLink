992cba23e2074cda5ac43d949f6b2dca74567eff
ChengTze-Wu <yaoop3050777@gmail.com>
2024-01-26 18:02:40 +0800

    Refactor JWTService import in cli.py

--- web_api/app/tools/cli.py
+++ web_api/app/tools/cli.py
@@ -1,5 +1,5 @@
 import click
-from app.services.token_service import JWTService
+from app.services import JWTService
 
 
 '''
@@ -14,7 +14,7 @@ payload:
 @click.option("--name", "-n", default=None)
 @click.option("--username", "-u", default=None)
 @click.option("--email", "-e", default=None)
-@click.option("--role", "-r", default=None, required=True, type=click.Choice(["admin", "group_owner", "group_member", "line_bot"]))
+@click.option("--role", "-r", default=None, required=True, type=click.Choice(["admin", "group_owner", "group_member", "line_bot", "liff"]))
 @click.option("--exp", "-x", type=int, default=None, help="Expire time in minutes")
 def create_jwt(name, username, email, role, exp):
     jwt_service = JWTService()
