6d11dab00ad933c5a0cebae5c8727bb6a3593ff1
ChengTze-Wu <yaoop3050777@gmail.com>
2024-01-28 21:20:50 +0800

    Update messages.py with new strings

--- bot-server/app/messages.py
+++ bot-server/app/messages.py
@@ -6,9 +6,10 @@ class ViewMessage:
     WELCOME_MEMBER_JOIN = "歡迎加入"
     BOT_JOIN_SUCCESS = "TimeLink Bot 加入成功!\n請輸入 tl 以查看各項功能。"
     USER_NOT_LINKED = "請點選 TimeLink 頭像加入好友！並完成註冊。"
+    NO_RECORD = "你尚未有預約"
+    NO_SERVICE = "本群組尚未有服務"
     
     # template messages
     WELCOME_NEW_USER = Template("歡迎, 您尚未加入系統, 請至 LIFF 註冊.")
     GROUP_NOT_LINKED = Template("此群組尚未連結 Bot，請至 https://timelink.chengtze.cc 群組管理輸入群組 ID：$line_group_id")
     RECORD = Template("你共有 $count 筆預約\n$appointments")
-    NO_RECORD = "你尚未有預約"
