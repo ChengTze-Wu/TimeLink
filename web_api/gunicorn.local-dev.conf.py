5b46da1882b5d1f880c58ef7742dc6442fd47219
ChengTze-Wu <yaoop3050777@gmail.com>
2024-02-05 23:25:40 +0800

    Add Dockerfile for local development and update gunicorn configuration

--- web_api/gunicorn.local-dev.conf.py
+++ web_api/gunicorn.local-dev.conf.py
@@ -4,8 +4,8 @@ bind = "0.0.0.0:8000"
 workers = 1
 
 # Logging
-accesslog = "-"
-errorlog = "-"
+accesslog = "app/logs/access.log"
+errorlog = "app/logs/error.log"
 
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 loglevel = "info"
