7932cd533578475d0719aa367a3c78499d7796ae
ChengTze-Wu <yaoop3050777@gmail.com>
2024-02-14 13:30:22 +0800

    Add main server code and run script

--- bot-server/app/main.py
+++ bot-server/app/main.py
@@ -231,3 +231,8 @@ async def __check_group_linked(event: Event):
         return False
 
     return False
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT', 8000)))
