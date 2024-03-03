4c751a9775c8270369d40244be166441f7900c53
ChengTze-Wu <yaoop3050777@gmail.com>
2024-03-03 15:57:50 +0800

    Refactor ComingRecordCommand to simplify code

--- bot-server/app/commands/record.py
+++ bot-server/app/commands/record.py
@@ -18,108 +18,26 @@ class ComingRecordCommand(Command):
         self.fetch_client = APIServerFetchClient()
 
     async def async_execute(self) -> TextMessage | None:
-        user_response = await self.__get_user()
-        handled_response = await self.__handle_response(user_response)
+        the_comming_appointment = await self.__get_the_comming_appointment()
+        the_comming_appointments_json: dict = the_comming_appointment.json()
 
-        if not handled_response:
-            return None
+        if the_comming_appointment.status_code != HTTPStatus.OK:
+             return TextMessage(text=ViewMessage.NO_COMING_RECORD)
 
-        if isinstance(handled_response, ViewMessage):
-            return TextMessage(text=handled_response)
-        
-        user_json: dict = user_response.json()
-        coming_appointment_message = await self.__format_coming_appointment(user_json)
+        reserved_at = the_comming_appointments_json.get('reserved_at')
+        format_reserved_at = datetime.strptime(reserved_at, '%a, %d %b %Y %H:%M:%S %Z')
+        service = the_comming_appointments_json.get('service')
+        service_name = service.get('name')
+        price = service.get('price')
 
-        return TextMessage(text=coming_appointment_message)
+        return TextMessage(text=ViewMessage.MOST_COMING_RECORD.substitute(
+            service_name=service_name,
+            reserved_at=format_reserved_at,
+            price=price
+        ))
     
-    async def __get_user(self):
+    async def __get_the_comming_appointment(self):
         line_user_id: str = self.event.source.user_id
-        return await self.fetch_client.get(f'/api/users/{line_user_id}')
-    
-    async def __get_service(self, service_id: str):
-        return await self.fetch_client.get(f'/api/services/{service_id}')
-    
-    async def __format_coming_appointment(self, user_json: dict):
-        appointments: list[dict] = user_json.get("appointments")
-        
-        most_coming_appointment = None
-        now = datetime.now()
-        for appointment in appointments:
-            reserved_at = datetime.strptime(appointment.get("reserved_at"), "%a, %d %b %Y %H:%M:%S %Z")
-            if reserved_at >= now:
-                if not most_coming_appointment:
-                    most_coming_appointment = appointment
-                most_coming_reserved_at = datetime.strptime(most_coming_appointment.get("reserved_at"), "%a, %d %b %Y %H:%M:%S %Z")
-                if reserved_at < most_coming_reserved_at:
-                    most_coming_appointment = appointment
-
-        if not most_coming_appointment:
-            return ViewMessage.NO_COMING_RECORD
-        
-        service_response = await self.__get_service(most_coming_appointment.get("service_id"))
-        service: dict = service_response.json()
-        format_reserved_at = datetime.strptime(most_coming_appointment.get("reserved_at"), "%a, %d %b %Y %H:%M:%S %Z").strftime("%Y-%m-%d %H:%M:%S")
-
-        return ViewMessage.MOST_COMING_RECORD.substitute(service_name=service.get("name"),
-                                                         reserved_at=format_reserved_at,
-                                                         price=service.get("price"))
-    
-    async def __handle_response(self, response) -> ViewMessage | None:
-        if response.status_code == HTTPStatus.NOT_FOUND:
-            return ViewMessage.USER_NOT_LINKED
-        
-        if response.status_code == HTTPStatus.OK:
-            return response
-
-        return None
-
-
-class AllRecordCommand(Command):
-    '''RecordCommand is a class that handles the user record command. It
-    will fetch the user's record from the API server and return it to the
-    user.
-    '''
-    def __init__(self, event):
-        super().__init__(event)
-        self.fetch_client = APIServerFetchClient()
-
-    async def async_execute(self) -> TextMessage | None:
-        user_response = await self.__get_user()
-        handled_response = await self.__handle_response(user_response)
-
-        if not handled_response:
-            return None
-
-        if isinstance(handled_response, ViewMessage):
-            return TextMessage(text=handled_response)
-        
-        user_json: dict = user_response.json()
-        appointment_message = await self.__format_appointments(user_json)
-
-        return TextMessage(text=appointment_message)
-    
-    async def __get_user(self):
-        line_user_id: str = self.event.source.user_id
-        return await self.fetch_client.get(f'/api/users/{line_user_id}')
-    
-    async def __format_appointments(self, user_json: dict):
-        appointments = user_json.get("appointments")
-
-        if not appointments:
-            return ViewMessage.NO_RECORD
-
-        formatted_appointments = "\n".join(
-            f"{appointment['reserved_at']}" for appointment in appointments
+        return await self.fetch_client.get(
+            f'/api/users/{line_user_id}/appointments?coming=true'
         )
-
-        return ViewMessage.RECORD.substitute(count=len(appointments),
-                                             appointments=formatted_appointments)
-    
-    async def __handle_response(self, response) -> ViewMessage | None:
-        if response.status_code == HTTPStatus.NOT_FOUND:
-            return ViewMessage.USER_NOT_LINKED
-        
-        if response.status_code == HTTPStatus.OK:
-            return response
-
-        return None
