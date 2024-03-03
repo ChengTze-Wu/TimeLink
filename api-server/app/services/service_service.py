3048ce0619b60f2e04d5f12b1c3dddabd6283581
ChengTze-Wu <yaoop3050777@gmail.com>
2024-03-03 16:03:22 +0800

    Refactor service_router.py to include JWT authentication

--- api-server/app/services/service_service.py
+++ api-server/app/services/service_service.py
@@ -5,11 +5,9 @@ from app.repositories import (
     UnavailablePeriodRepository,
     GroupRepository,
 )
-from .token_service import JWTService
 from app.db.models import DayOfWeek
-from werkzeug.exceptions import BadRequest
+from werkzeug.exceptions import BadRequest, NotFound, Forbidden
 from datetime import datetime
-from uuid import UUID
 
 
 class ServiceService:
@@ -18,16 +16,23 @@ class ServiceService:
         self.group_repository = GroupRepository()
         self.working_hour_repository = WorkingHourRepository()
         self.unavailable_period_repository = UnavailablePeriodRepository()
-        self.payload = JWTService().get_payload()
 
-    def create_one(self, service_json_data: dict) -> dict:
+    def create_one(self, service_json_data: dict, payload: dict | None = {}) -> dict:
         working_hours = service_json_data.get("working_hours", []) or []
         unavailable_periods = service_json_data.get("unavailable_periods", []) or []
         group_ids = service_json_data.get("groups")
-        user_id = self.payload.get("sub")
+        user_id = payload.get("sub")
+        role = payload.get("role")
 
         if user_id is None:
             raise BadRequest("sub must be provided in JWT payload")
+
+        is_user_owns_groups = self.group_repository.check_groups_owner_by_user_id(
+            user_id=user_id, group_ids=group_ids
+        )
+
+        if role not in ["admin"] and not is_user_owns_groups:
+            raise Forbidden("You are not the owner of one of the groups")
 
         # Validate working_hours
         days_of_week = set()
@@ -77,36 +82,80 @@ class ServiceService:
         per_page: int = 10,
         query: str = None,
         status: int = None,
+        line_group_id: str = None,
         with_total_items: bool = False,
+        payload: dict | None = {}
     ) -> List[dict] | Tuple[List[dict], int]:
-        role = self.payload.get("role")
-        owner_id = self.payload.get("sub") if role != "admin" else None
+        '''
+        'group_owner' can only view services in their groups, identified by 'owner_id'.
+        'line_bot' and 'liff' can only view services in their groups, identified by 'line_group_id'.
+        'admin' can view all services.
+        '''
+        role = payload.get("role")
+        owner_id = payload.get("sub") if role != "admin" else None
+        group_id = self.__convert_line_group_id_to_group_id(line_group_id) if line_group_id else None
         services = self.service_repository.select_all_by_filter(
-            page, per_page, query, status, owner_id
+            page, per_page, query, status, owner_id, group_id
         )
         if with_total_items:
             total_items_count = self.service_repository.count_all_by_filter(
-                query, status, owner_id
+                query, status, owner_id, group_id
             )
             return services, total_items_count
         return services
 
-    def get_one(self, service_id: str) -> dict:
-        role = self.payload.get("role")
+    def get_most_popular_by_line_group_id(self, line_group_id: str, limit: int = 5) -> List[dict]:
+        return self.service_repository.select_most_popular_by_line_group_id(line_group_id, limit)
 
-        if role == "liff" or role == "line_bot":
+    def get_one(self, service_id: str, payload: dict | None = {}) -> dict:
+        role = payload.get("role")
+        if role in ["liff", "line_bot", "admin"]:
             return self.service_repository.select_one_by_unique_filed(
                 service_id=service_id
             )
 
-        return self.__retrieve_service_by_owner(service_id, role)
+        user_id = payload.get("sub")
 
-    def delete_one(self, service_id: str) -> dict:
-        self.__retrieve_service_by_owner(service_id)
+        if role in ["group_member"]:
+            is_user_in_group = self.service_repository.check_if_user_in_group_by_service_id(
+                service_id=service_id, user_id=user_id
+            )
+            if not is_user_in_group:
+                raise Forbidden("You are not in the group of this service")
+            
+        if role in ["group_owner"]:
+            is_user_own_service = self.service_repository.check_if_user_own_service(
+                service_id=service_id, user_id=user_id
+            )
+            if not is_user_own_service:
+                raise Forbidden("You are not the owner of service")
+
+        return self.service_repository.select_one_by_unique_filed(
+            service_id=service_id
+        )
+
+    def delete_one(self, service_id: str, payload: dict | None = {}) -> dict:
+        user_id = payload.get("sub")
+        role = payload.get("role")
+
+        is_user_own_service = self.service_repository.check_if_user_own_service(
+            service_id=service_id, user_id=user_id
+        )
+
+        if role not in ["admin"] and not is_user_own_service:
+            raise Forbidden("You are not the owner of service")
+
         return self.service_repository.logical_delete_one_by_id(service_id)
 
-    def update_one(self, service_id: str, service_json_data: dict) -> dict:
-        self.__retrieve_service_by_owner(service_id)
+    def update_one(self, service_id: str, service_json_data: dict, payload: dict | None = {}) -> dict:
+        user_id = payload.get("sub")
+        role = payload.get("role")
+
+        is_user_own_service = self.service_repository.check_if_user_own_service(
+            service_id=service_id, user_id=user_id
+        )
+        if role not in ["admin"] and not is_user_own_service:
+            raise Forbidden("You are not the owner of service")
 
         input_unavailable_periods = service_json_data.get("unavailable_periods", [])
         input_working_hours = service_json_data.get("working_hours", [])
@@ -127,12 +176,8 @@ class ServiceService:
             input_working_hours,
         )
 
-    def __retrieve_service_by_owner(self, service_id: str, role: str | None = None):
-        service_data = self.service_repository.select_one_by_unique_filed(
-            service_id=service_id
+    def __convert_line_group_id_to_group_id(self, line_group_id: str):
+        group_data = self.group_repository.select_one_by_unique_filed(
+            line_group_id=line_group_id
         )
-        if role != "admin" and self.payload.get("sub") != str(
-            service_data.get("owner").get("id")
-        ):
-            raise BadRequest("You are not the owner of this service")
-        return service_data
+        return group_data.get("id")
