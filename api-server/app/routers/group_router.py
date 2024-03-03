46c54bb6d692139268de9f7628d82a7ed2796e5e
ChengTze-Wu <yaoop3050777@gmail.com>
2024-03-03 16:03:02 +0800

    Refactor GroupRepository class in group_repository.py

--- api-server/app/routers/group_router.py
+++ api-server/app/routers/group_router.py
@@ -1,20 +1,24 @@
 from flask import Blueprint, request, abort
 from app.utils.validators.request_vaildator import RequestValidator
 from app.views.response_view import RESTfulResponse
-from app.services import GroupService
+from app.services import GroupService, ServiceService, UserService
 from werkzeug.exceptions import HTTPException
 from requests.exceptions import RequestException
 from app.utils.validators.token_validator import verify_access_token
+from app.utils.handlers.jwt_handler import JWTHandler
 
 
 bp = Blueprint("group_router", __name__)
+group_service = GroupService()
+service_service = ServiceService()
+user_service = UserService()
+jwt_handler = JWTHandler()
 
 
 @bp.route("", methods=["POST"])
 @verify_access_token(access_roles=["group_owner"])
 def link_bot_endpoint():
     try:
-        group_service = GroupService()
         request_validator = RequestValidator(
             request_type="json",
             required_fields=["line_group_id"],
@@ -25,7 +29,8 @@ def link_bot_endpoint():
         )
         request_validator.check(request)
         group_json_data = request.get_json()
-        created_group_data = group_service.create_one(group_json_data)
+        jwt_payload = jwt_handler.get_payload(request.headers.get("Authorization"))
+        created_group_data = group_service.create_one(group_json_data, payload=jwt_payload)
         return RESTfulResponse(created_group_data).to_serializable(), 201
     except HTTPException as e:
         abort(e.code, e.description)
@@ -35,22 +40,13 @@ def link_bot_endpoint():
         abort(500, e)
 
 
-@bp.route("/users", methods=["POST"])
+@bp.route("/<line_group_id>/users/<line_user_id>", methods=["POST"])
 @verify_access_token(access_roles=["line_bot"])
-def link_user_endpoint():
+def link_user_endpoint(line_group_id, line_user_id):
     try:
-        group_service = GroupService()
-        request_validator = RequestValidator(
-            request_type="json",
-            required_fields=["line_user_id", "line_group_id"],
-            field_types={
-                "line_user_id": str,
-                "line_group_id": str,
-            },
+        created_group_data = group_service.link_user_to_group(
+            line_group_id=line_group_id, line_user_id=line_user_id
         )
-        request_validator.check(request)
-        group_json_data = request.get_json()
-        created_group_data = group_service.link_user_to_group(group_json_data)
         return RESTfulResponse(created_group_data).to_serializable(), 201
     except HTTPException as e:
         abort(e.code, e.description)
@@ -60,12 +56,13 @@ def link_user_endpoint():
         abort(500, e)
 
 
-@bp.route("{line_group_id}/users/{line_user_id}", methods=["DELETE"])
+@bp.route("/<line_group_id>/users/<line_user_id>", methods=["DELETE"])
 @verify_access_token(access_roles=["line_bot"])
 def unlink_user_endpoint(line_group_id, line_user_id):
     try:
-        group_service = GroupService()
-        deleted_group_data = group_service.unlink_user_from_group(line_group_id=line_group_id, line_user_id=line_user_id)
+        deleted_group_data = group_service.unlink_user_from_group(
+            line_group_id=line_group_id, line_user_id=line_user_id
+        )
         return RESTfulResponse(deleted_group_data).to_serializable()
     except HTTPException as e:
         abort(e.code, e.description)
@@ -79,17 +76,18 @@ def unlink_user_endpoint(line_group_id, line_user_id):
 @verify_access_token(access_roles=["admin", "group_owner"])
 def get_all_endpoint():
     try:
-        group_service = GroupService()
         page = request.args.get("page", 1, type=int)
         per_page = request.args.get("per_page", 10, type=int)
         query = request.args.get("query", None, type=str)
         status = request.args.get("status", None, type=int)
+        jwt_payload = jwt_handler.get_payload(request.headers.get("Authorization"))
         group_dataset, total_items_count = group_service.get_all(
             page=page,
             per_page=per_page,
             query=query,
             status=status,
             with_total_items=True,
+            payload=jwt_payload,
         )
         return RESTfulResponse(
             group_dataset, pagination=(page, per_page, total_items_count)
@@ -104,8 +102,21 @@ def get_all_endpoint():
 @verify_access_token(access_roles=["admin", "group_owner"])
 def get_one_endpoint(group_id):
     try:
-        group_service = GroupService()
-        group_data = group_service.get_one(group_id=group_id)
+        print(type(group_id))
+        jwt_payload = jwt_handler.get_payload(request.headers.get("Authorization"))
+        group_data = group_service.get_one(group_id=group_id, payload=jwt_payload)
+        return RESTfulResponse(group_data).to_serializable()
+    except HTTPException as e:
+        abort(e.code, e.description)
+    except Exception as e:
+        abort(500, e)
+
+
+@bp.route("/line/<line_group_id>", methods=["GET"])
+@verify_access_token(access_roles=["line_bot", "liff"])
+def get_line_group_endpoint(line_group_id):
+    try:
+        group_data = group_service.get_one(line_group_id=line_group_id)
         return RESTfulResponse(group_data).to_serializable()
     except HTTPException as e:
         abort(e.code, e.description)
@@ -115,26 +126,39 @@ def get_one_endpoint(group_id):
 
 @bp.route("/<line_group_id>/services", methods=["GET"])
 @verify_access_token(access_roles=["line_bot", "liff"])
-def get_line_group_services_endpoint(line_group_id):
+def get_group_services_endpoint(line_group_id):
     try:
-        group_service = GroupService()
-        group_data = group_service.get_one_with_appointments(
-            line_group_id=line_group_id
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 10, type=int)
+        popular = request.args.get("popular", None, type=int)
+
+        if popular:
+            service_dataset = service_service.get_most_popular_by_line_group_id(
+                line_group_id=line_group_id, limit=popular
+            )
+            return RESTfulResponse(service_dataset).to_serializable()
+
+        service_dataset, total_items_count = service_service.get_all(
+            line_group_id=line_group_id, with_total_items=True
         )
-        return RESTfulResponse(group_data).to_serializable()
+        return RESTfulResponse(service_dataset, pagination=(page, per_page, total_items_count)).to_serializable()
     except HTTPException as e:
         abort(e.code, e.description)
     except Exception as e:
         abort(500, e)
 
 
-@bp.route("/<line_group_id>", methods=["GET"])
-@verify_access_token(access_roles=["line_bot", "liff"])
-def get_line_group_endpoint(line_group_id):
+@bp.route("/<uuid:group_id>/users", methods=["GET"])
+@verify_access_token(access_roles=["admin", "group_owner"])
+def get_group_users_endpoint(group_id):
     try:
-        group_service = GroupService()
-        group_data = group_service.get_one(line_group_id=line_group_id)
-        return RESTfulResponse(group_data).to_serializable()
+        jwt_payload = jwt_handler.get_payload(request.headers.get("Authorization"))
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 10, type=int)
+        service_dataset, total_items_count = user_service.get_all(
+            group_id=group_id, with_total_items=True, page=page, per_page=per_page, payload=jwt_payload
+        )
+        return RESTfulResponse(service_dataset, pagination=(page, per_page, total_items_count)).to_serializable()
     except HTTPException as e:
         abort(e.code, e.description)
     except Exception as e:
@@ -145,7 +169,6 @@ def get_line_group_endpoint(line_group_id):
 @verify_access_token(access_roles=["admin", "group_owner"])
 def update_endpoint(group_id):
     try:
-        group_service = GroupService()
         request_validator = RequestValidator(
             request_type="json",
             field_types={
@@ -154,7 +177,8 @@ def update_endpoint(group_id):
         )
         request_validator.check(request)
         group_json_data = request.get_json()
-        updated_group_data = group_service.update_one(group_id, group_json_data)
+        jwt_payload = jwt_handler.get_payload(request.headers.get("Authorization"))
+        updated_group_data = group_service.update_one(group_id, group_json_data, payload=jwt_payload)
         return RESTfulResponse(updated_group_data).to_serializable()
     except HTTPException as e:
         abort(e.code, e.description)
@@ -166,8 +190,8 @@ def update_endpoint(group_id):
 @verify_access_token(access_roles=["admin", "group_owner"])
 def delete_endpoint(group_id):
     try:
-        group_service = GroupService()
-        deleted_group_data = group_service.delete_one(group_id)
+        jwt_payload = jwt_handler.get_payload(request.headers.get("Authorization"))
+        deleted_group_data = group_service.delete_one(group_id, payload=jwt_payload)
         return RESTfulResponse(deleted_group_data).to_serializable()
     except HTTPException as e:
         abort(e.code, e.description)
--- api-server/app/services/group_service.py
+++ api-server/app/services/group_service.py
@@ -2,12 +2,8 @@ import os
 import requests
 from werkzeug.exceptions import NotFound, BadRequest, Forbidden
 from typing import List, Tuple
-from app.repositories import (
-    GroupRepository,
-    UserRepository,
-    ServiceRepository,
-)
-from .token_service import JWTService
+from app.repositories import GroupRepository
+
 
 CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 if CHANNEL_ACCESS_TOKEN is None:
@@ -15,15 +11,88 @@ if CHANNEL_ACCESS_TOKEN is None:
 
 
 class GroupService:
-    """This class must be initialized within a Flask view function because it needs to retrieve
-    the payload from the JWT, which is placed in the request header.
-    """
-
     def __init__(self):
         self.group_repository = GroupRepository()
-        self.user_repository = UserRepository()
-        self.service_repository = ServiceRepository()
-        self.payload = JWTService().get_payload()
+
+    def create_one(self, group_json_data: dict, payload: dict | None = {}) -> dict:
+        line_group_id = group_json_data.get("line_group_id")
+        line_group_summary = self.__fetch_group_summary_from_lineapi(line_group_id)
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise BadRequest("sub must be provided in JWT payload")
+
+        new_group_data = {
+            "name": line_group_summary.get("groupName"),
+            "line_group_id": line_group_id,
+            "is_active": group_json_data.get("is_active", True),
+            "owner_id": user_id,
+        }
+        return self.group_repository.insert_one(new_group_data)
+
+    def get_one(self, group_id: str = None, line_group_id: str = None, payload: dict | None = {}) -> dict:
+        if group_id:
+            return self.__retrieve_group_by_owner(group_id, payload)
+
+        if line_group_id:
+            return self.group_repository.select_one_by_unique_filed(
+                line_group_id=line_group_id
+            )
+
+        raise BadRequest("group_id or line_group_id must be provided")
+
+    def get_all(
+        self,
+        page: int = 1,
+        per_page: int = 10,
+        query: str = None,
+        status: int = None,
+        service_id: str = None,
+        with_total_items: bool = False,
+        payload: dict | None = {},
+    ) -> List[dict] | Tuple[List[dict], int]:
+        role = payload.get("role")
+        owner_id = payload.get("sub") if role != "admin" else None
+        list_dict_groups = self.group_repository.select_all_by_filter(
+            page, per_page, query, status, owner_id, service_id=service_id, with_by="owner"
+        )
+        if with_total_items:
+            total_items_count = self.group_repository.count_all_by_filter(
+                query, status, owner_id
+            )
+            return list_dict_groups, total_items_count
+
+        return list_dict_groups
+
+    def update_one(self, group_id: str, group_json_data: dict, payload: dict | None = {}) -> dict:
+        self.__retrieve_group_by_owner(group_id, payload)
+        update_group_data = {
+            "is_active": group_json_data.get("is_active"),
+        }
+        return self.group_repository.update_one_by_id(group_id, update_group_data)
+
+    def delete_one(self, group_id: str, payload: dict | None = {}) -> dict:
+        self.__retrieve_group_by_owner(group_id, payload)
+        return self.group_repository.logical_delete_one_by_id(group_id)
+
+    def link_user_to_group(self, line_group_id: str, line_user_id: str) -> dict:
+        return self.group_repository.add_user_to_group_by_line_ids(
+            line_group_id, line_user_id
+        )
+
+    def unlink_user_from_group(self, line_group_id: str, line_user_id: str) -> dict:
+        return self.group_repository.remove_user_from_group_by_line_ids(
+            line_group_id, line_user_id
+        )
+
+    def __retrieve_group_by_owner(self, group_id: str, payload: dict | None = {}):
+        role = payload.get("role")
+        group_data = self.group_repository.select_one_by_unique_filed(group_id=group_id)
+        if role != "admin" and payload.get("sub") != str(
+            group_data.get("owner").get("id")
+        ):
+            raise Forbidden("You are not the owner of this group")
+        return group_data
 
     def __fetch_group_summary_from_lineapi(self, line_group_id: str) -> dict:
         line_api_endpoint = f"https://api.line.me/v2/bot/group/{line_group_id}/summary"
@@ -37,102 +106,3 @@ class GroupService:
             raise NotFound(response.json()["message"])
         response.raise_for_status()
         return response.json()
-
-    def create_one(self, group_json_data: dict) -> dict:
-        line_group_id = group_json_data.get("line_group_id")
-        line_group_summary = self.__fetch_group_summary_from_lineapi(line_group_id)
-        user_id = self.payload.get("sub")
-
-        if user_id is None:
-            raise BadRequest("sub must be provided in JWT payload")
-
-        new_group_data = {
-            "name": line_group_summary.get("groupName"),
-            "line_group_id": line_group_id,
-            "is_active": group_json_data.get("is_active", True),
-            "owner_id": user_id,
-        }
-        return self.group_repository.insert_one(new_group_data)
-
-    def get_one(self, group_id: str = None, line_group_id: str = None) -> dict:
-        if group_id:
-            return self.__retrieve_group_by_owner(group_id)
-
-        if line_group_id:
-            return self.group_repository.select_one_by_unique_filed(
-                line_group_id=line_group_id
-            )
-
-        raise BadRequest("group_id or line_group_id must be provided")
-
-    def get_one_with_appointments(self, line_group_id: str) -> dict:
-        group_data = self.group_repository.select_one_by_unique_filed(
-            line_group_id=line_group_id
-        )
-        if group_data is None:
-            raise NotFound("Group not found")
-
-        services = group_data.get("services")
-
-        group_data["services"] = []
-
-        for service in services:
-            service = self.service_repository.select_one_by_unique_filed(
-                service.get("id")
-            )
-            group_data["services"].append(service)
-
-        return group_data
-
-    def get_all(
-        self,
-        page: int = 1,
-        per_page: int = 10,
-        query: str = None,
-        status: int = None,
-        with_total_items: bool = False,
-    ) -> List[dict] | Tuple[List[dict], int]:
-        role = self.payload.get("role")
-        owner_id = self.payload.get("sub") if role != "admin" else None
-        list_dict_groups = self.group_repository.select_all_by_filter(
-            page, per_page, query, status, owner_id
-        )
-        if with_total_items:
-            total_items_count = self.group_repository.count_all_by_filter(
-                query, status, owner_id
-            )
-            return list_dict_groups, total_items_count
-
-        return list_dict_groups
-
-    def update_one(self, group_id: str, group_json_data: dict) -> dict:
-        self.__retrieve_group_by_owner(group_id)
-        update_group_data = {
-            "is_active": group_json_data.get("is_active"),
-        }
-        return self.group_repository.update_one_by_id(group_id, update_group_data)
-
-    def delete_one(self, group_id: str) -> dict:
-        self.__retrieve_group_by_owner(group_id)
-        return self.group_repository.logical_delete_one_by_id(group_id)
-
-    def link_user_to_group(self, group_json_data: dict) -> dict:
-        line_group_id = group_json_data.get("line_group_id")
-        line_user_id = group_json_data.get("line_user_id")
-        return self.group_repository.add_user_to_group_by_line_ids(
-            line_group_id, line_user_id
-        )
-
-    def unlink_user_from_group(self, line_group_id: str, line_user_id: str) -> dict:
-        return self.group_repository.remove_user_from_group_by_line_ids(
-            line_group_id, line_user_id
-        )
-
-    def __retrieve_group_by_owner(self, group_id: str):
-        role = self.payload.get("role")
-        group_data = self.group_repository.select_one_by_unique_filed(group_id=group_id)
-        if role != "admin" and self.payload.get("sub") != str(
-            group_data.get("owner").get("id")
-        ):
-            raise Forbidden("You are not the owner of this group")
-        return group_data
