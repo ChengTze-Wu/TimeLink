3142e1474d1324df9352167a9a7c193121fda2f4
ChengTze-Wu <yaoop3050777@gmail.com>
2024-03-06 15:08:41 +0800

    Update welcome message to include instructions for accessing features

--- bot-server/app/messages.py
+++ bot-server/app/messages.py
@@ -3,7 +3,7 @@ from string import Template
 class ViewMessage:
     # static messages
     WELCOME_BACK = "歡迎回來"
-    WELCOME_MEMBER_JOIN = "歡迎加入"
+    WELCOME_MEMBER_JOIN = "歡迎加入\n請輸入`/`以查看各項功能。"
     BOT_JOIN_SUCCESS = "TimeLink Bot 加入成功!\n請輸入`/`以查看各項功能。"
     USER_NOT_LINKED = "請點選 TimeLink 頭像加入好友！並完成註冊。"
     NO_RECORD = "你尚未有預約"
